fb0e094f60cc430a7857515cf875c5a7bba5328e
Mark Repka <mark@repkam09.com>
2023-02-21 21:46:50 +0000

    Adjust prompt

--- index.js
+++ index.js
@@ -36,15 +36,17 @@ async function init() {
 
         console.log("ChatId", chatId, "Message:", text)
         try {
+	    const prompt = `Elaborate on the following, providing a friendly and helpful answer, writing at a college educated level: ${text}`
+       	    console.log("ChatId", chatId, "Prompt:", prompt)
+
             const response = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: ${text}\nA:`,
-                temperature: 0,
-                max_tokens: 100,
-                top_p: 1,
-                frequency_penalty: 0.0,
-                presence_penalty: 0.0,
-                stop: ["\n"],
+		model: "text-davinci-003",
+		prompt: prompt,
+		temperature: 0.7,
+		max_tokens: 512,
+		top_p: 1,
+		frequency_penalty: 0,
+  		presence_penalty: 0,
             });
 
             console.log("ChatId", chatId, "Response:", response.data.id)
