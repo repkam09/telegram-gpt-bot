8c941c4c392124277d8a6013ac90db6480170e51
Mark Repka <mrepka@bitovi.com>
2023-02-22 14:29:41 -0500

    Add chat context

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
+const chatContextMap = new Map();
 
-function buildConfig(name, text) {
+function buildConfig(prompt) {
     return {
         model: "text-davinci-003",
-        prompt: buildPrompt(name, text),
+        prompt: prompt,
         temperature: 0.7,
         max_tokens: 512,
         top_p: 1,
@@ -17,23 +18,22 @@ function buildConfig(name, text) {
     }
 }
 
-function buildPrompt(name, text) {
-    return `You are a conversational chat assistant named 'Hennos' that is helpful, creative, clever, and friendly. Here is an example conversation between Hennos and the user named ${name}.
 
-${name}: What can you help with? 
+function buildPrompt(chatId, firstName, message) {
+    if (!chatContextMap.has(chatId)) {
 
-Hennos: I'm Hennos, your friendly chat assistant here to help answer your questions. I'm knowledgeable about a wide range of topics and can provide you with helpful information and answer your questions.
-    
-${name}: What is the James Webb Space Telescope?
+        const promptPrefix = `You are a conversational chat assistant named 'Hennos' that is helpful, creative, clever, and friendly. Here is a conversation between Hennos and the user named '${firstName}':
+${firstName}: Tell me about yourself
+Hennos: I'm Hennos, your friendly chat assistant! I'm here to answer questions and help you find the information you need. I'm knowledgeable on a wide range of topics, so feel free to ask away!
+${firstName}: What can you help with?
+Hennos: I'm knowledgeable about a wide range of topics and can provide you with helpful information and provide answers to your questions.`
+        chatContextMap.set(chatId, promptPrefix)
+    }
 
-Hennos: The James Webb Space Telescope (JWST) is a space telescope currently conducting infrared astronomy. As the largest optical telescope in space, it is equipped with high-resolution and high-sensitivity instruments, allowing it to view objects too old, distant, or faint for the Hubble Space Telescope. This enables investigations across many fields of astronomy and cosmology, such as observation of the first stars, the formation of the first galaxies, and detailed atmospheric characterization of potentially habitable exoplanets.
+    const chatContext = chatContextMap.get(chatId)
 
-${name}: Who is George Lucas?
-
-Hennos: George Walton Lucas Jr. is an American filmmaker. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, LucasArts, Industrial Light & Magic and THX. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012.
-
-${name}: ${text}    
-
+    return `${chatContext}
+${firstName}: ${message}
 Hennos: `
 }
 
@@ -49,16 +49,20 @@ async function init() {
 
     bot.on('message', async (msg) => {
         const chatId = msg.chat.id;
-        const text = msg.text
+        const message = msg.text
+
+        if (!message) {
+            return
+        }
 
         // If the incoming text is empty or a command, ignore it for the moment
-        if (!text || text.startsWith('/')) {
+        if (message.startsWith('/')) {
             return
         }
 
         // Pull out some identifiers for helpful logging
-        const username = msg.chat.first_name || "Telegram User"
-        const identifier = `${username} (${chatId})`
+        const firstName = msg.chat.first_name || "Telegram User"
+        const identifier = `${firstName} (${chatId})`
 
         // If a whitelist is provided check that the incoming chatId is in the list
         if (process.env.TELEGRAM_ID_WHITELIST) {
@@ -71,16 +75,40 @@ async function init() {
         }
 
         // Log the incoming request
-        console.log(identifier, ":", text)
+        console.log(identifier, ":", message)
 
         // Ask OpenAI for the text completion and return the results to Telegram
         try {
-            const response = await openai.createCompletion(buildConfig(username, text));
+            const prompt = buildPrompt(chatId, firstName, message)
+
+            console.log(prompt)
+
+            const response = await openai.createCompletion(buildConfig(prompt));
             if (response.data && response.data.choices) {
-                bot.sendMessage(chatId, response.data.choices[0].text);
+
+                // Extract the bot response 
+                const result = response.data.choices[0].text
+
+                // Send the response to the user
+                bot.sendMessage(chatId, result);
+
+                // Update our existing chat context with the result of this completion
+                const chatContext = chatContextMap.get(chatId)
+                const updatedChatContext = `${chatContext}
+${firstName}: ${message}
+Hennos: ${result}
+`
+
+                chatContextMap.set(chatId, updatedChatContext)
             }
         } catch (err) {
             bot.sendMessage(chatId, 'Error: ' + err.message);
+
+            // Clean up the chat context when something goes wrong, just in case...
+            if (chatContextMap.has(chatId)) {
+                chatContextMap.delete(chatId)
+            }
+
             console.log("ChatId", chatId, "Response Error:", err.message)
             console.log(err)
         }
