5236a3fd9e577e9cabadf1e48adadcbccc2d4b52
Mark Repka <mrepka@bitovi.com>
2023-04-28 12:49:50 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo service repka-gpt stop
 git pull
 npm i
-npm run build
-sudo service repka-gpt start
+npm run validate && npm run build
+sudo service repka-gpt restart
 sudo service repka-gpt status
