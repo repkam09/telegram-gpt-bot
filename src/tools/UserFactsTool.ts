d05ff9cd6a944457f26c95945b6d6e62fd3b5d3f
Mark Repka <mark.repka@yum.com>
2024-10-30 12:33:10 -0400

    ability to delete facts

--- src/tools/UserFactsTool.ts
+++ src/tools/UserFactsTool.ts
@@ -14,7 +14,8 @@ export class StoreKeyValueMemory extends BaseTool {
                     "Use this tool to store facts, information, or other important details related to a user.",
                     "This tool is designed to help the system remember and recall user-specific information for future interactions.",
                     "This information will automatically be made available to the system on future interactions with the same user.",
-                    "If the 'key' already exists, the 'value' will be updated with the new content provided."
+                    "If the 'key' already exists, the 'value' will be updated with the new content provided.",
+                    "If 'value' is not provided, the existing key-value pair will be removed.",
                 ].join(" "),
                 parameters: {
                     type: "object",
@@ -25,10 +26,10 @@ export class StoreKeyValueMemory extends BaseTool {
                         },
                         value: {
                             type: "string",
-                            description: "The value or content of the fact to be stored.",
+                            description: "The value or content of the fact to be stored. If omitted, the key-value pair will be removed.",
                         },
                     },
-                    required: ["key", "value"],
+                    required: ["key"],
                 }
             }
         };
@@ -40,26 +41,36 @@ export class StoreKeyValueMemory extends BaseTool {
             return ["store_key_value_memory, Error: key not provided.", metadata];
         }
 
-        if (!args.value) {
-            return ["store_key_value_memory, Error: value not provided.", metadata];
-        }
-
         try {
             const database = Database.instance();
-            await database.keyValueMemory.upsert({
-                where: {
-                    chatId_key: {
-                        chatId: req.chatId, key: args.key
+            if (args.value) {
+                await database.keyValueMemory.upsert({
+                    where: {
+                        chatId_key: {
+                            chatId: req.chatId, key: args.key
+                        }
+                    },
+                    update: { value: args.value },
+                    create: { key: String(args.key), value: String(args.value), chatId: req.chatId }
+                });
+                return [`store_key_value_memory, success! key='${args.key}', value='${args.value}'`, metadata];
+            }
+
+            if (!args.value) {
+                await database.keyValueMemory.delete({
+                    where: {
+                        chatId_key: {
+                            chatId: req.chatId, key: args.key
+                        }
                     }
-                },
-                update: { value: args.value },
-                create: { key: String(args.key), value: String(args.value), chatId: req.chatId }
-            });
+                });
+                return [`store_key_value_memory, success! key='${args.key}' was removed from the database.`, metadata];
+            }
+
         } catch (err) {
             Logger.error(req, "StoreKeyValueMemory unable to store information", { key: args.key, value: args.value, err: err });
-            return ["store_key_value_memory, Error: unable to store key value pair.", metadata];
         }
 
-        return [`store_key_value_memory, success! key='${args.key}', value='${args.value}'`, metadata];
+        return ["store_key_value_memory, Error: unable to store key value pair.", metadata];
     }
 }
