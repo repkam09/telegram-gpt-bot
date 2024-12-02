fbcd372b3e2b402c299ae805479a56df18f0a559
Mark Repka <mark@repkam09.com>
2024-12-01 20:38:00 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,20 @@
+# Pull the latest changes from the repository
 git pull
+
+# Build the docker image
 docker build -t hennos-gpt .
+
+# Stop and remove the current container
 docker compose down
+
+# Create backup directory if it doesn't exist
+mkdir -p ./data/bak
+
+# Get the current timestamp
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+# Create a backup of the SQLite database
+cp ./data/hennos.sqlite ./data/bak/hennos_${timestamp}.sqlite
+
+# Start the new container
 docker compose up -d
--- legacy_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git pull
-npm i
-npm run validate && npm run build
-sudo service repka-gpt restart
-sudo service repka-gpt status
